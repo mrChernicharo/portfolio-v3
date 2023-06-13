e6d70de2a2a658a2a1912d0357955f6d2beecd37
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2023-06-13 10:37:12 -0300

    typing animation works !

--- src/components/TypingAnimation.tsx
+++ src/components/TypingAnimation.tsx
@@ -1,13 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+
+type CodeColorInstruction = {
+  color: string;
+  start: number;
+  end: number;
+};
+
+type CodeChar = {
+  id: string;
+  char: string;
+  color: string;
+};
+
 // accent -> yellow
 // primary -> red
 // secondary -> purple
-const colorScheme = [
+const colorScheme: CodeColorInstruction[][] = [
   [
     // 0
     { color: "primary", start: 0, end: 5 },
     { color: "white", start: 6, end: 28 },
-    { color: "secondary", start: 29, end: 53 },
-    { color: "white", start: 54, end: 55 },
+    { color: "primary", start: 29, end: 33 },
+    { color: "accent", start: 34, end: 55 },
   ],
   [
     // 1
@@ -23,19 +37,17 @@ const colorScheme = [
     { color: "white", start: 49, end: 50 },
   ],
   [
-    { color: "accent", start: 0, end: 12 },
+    { color: "secondary", start: 0, end: 12 },
     { color: "white", start: 13, end: 15 },
   ],
   [
     // 3
     { color: "white", start: 0, end: 1 },
     { color: "primary", start: 2, end: 5 },
-    { color: "white", start: 6, end: 11 },
-    { color: "primary", start: 12, end: 12 },
-    { color: "white", start: 13, end: 14 },
-    { color: "accent", start: 15, end: 29 },
-    { color: "white", start: 30, end: 31 },
-    { color: "accent", start: 32, end: 34 },
+    { color: "white", start: 6, end: 9 },
+    { color: "primary", start: 10, end: 12 },
+    { color: "accent", start: 13, end: 27 },
+    { color: "white", start: 28, end: 28 },
   ],
   [
     // 4
@@ -46,8 +58,9 @@ const colorScheme = [
     { color: "white", start: 13, end: 14 },
     { color: "accent", start: 15, end: 29 },
     { color: "white", start: 30, end: 31 },
-    { color: "accent", start: 32, end: 34 },
-    { color: "accent", start: 32, end: 34 },
+    { color: "accent", start: 32, end: 50 },
+    { color: "white", start: 51, end: 51 },
+    { color: "accent", start: 52, end: 65 },
   ],
   [
     { color: "white", start: 0, end: 6 },
@@ -55,23 +68,23 @@ const colorScheme = [
     { color: "accent", start: 8, end: 57 },
     { color: "white", start: 58, end: 61 },
   ],
-  [
+  [ // 6
     { color: "primary", start: 0, end: 8 },
     { color: "secondary", start: 9, end: 23 },
     { color: "white", start: 24, end: 27 },
   ],
-  [{ color: "white", start: 0, end: 1 }],
+  [{ color: "white", start: 0, end: 2 }],
 ];
 
-const textTable = [
-  "import { SoftwareDeveloper } from 'felipe-chernicharo';",
+const codeLines = [
+  "import { FelipeChernicharo } from 'software-development';",
   "var helloStranger = `welcome to my ${Portfolio}!`;",
   "soGladYouHere() {",
   "  var take = 'a look around';",
   "  var check = ['my background', 'personal projects', 'tech skills'];",
   "  { see: 'a little bit of my work as a programmer, and...' };",
   "  return wheneverYouLike();",
-  "}",
+  "}; ",
 ];
 
 let objIdx = 0;
@@ -88,20 +101,69 @@ function getCharColor(id: string) {
   // if (col >= colorRow[objIdx].start && col <= colorRow[objIdx].end) {}
 }
 
-const codeTable: any = [];
-for (let i = 0; i < textTable.length; i++) {
-  const line = textTable[i];
+const codeTable: CodeChar[][] = [];
+for (let i = 0; i < codeLines.length; i++) {
+  const line = codeLines[i];
   codeTable[i] = [];
   for (let j = 0; j < line.length; j++) {
-    const [id, ch] = [`${i}-${j}`, line[j]];
-    codeTable[i][j] = { id, ch, color: getCharColor(id) };
+    const [id, char] = [`${i}-${j}`, line[j]];
+    codeTable[i][j] = { id, char, color: getCharColor(id) };
   }
 }
 
-console.log({ codeTable });
-
 export default function TypingAnimation() {
-  return <div>
-    
-  </div>;
+  const [cursorIdx, setCursorIdx] = useState(0);
+  const [cursorRow, setCursorRow] = useState(0);
+  const interval = useRef(0);
+
+  useEffect(() => {
+    interval.current = setInterval(() => {
+      setCursorIdx((prev) => prev + 1);
+    }, 20);
+
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (codeTable[cursorRow] && cursorIdx >= codeTable[cursorRow].length) {
+      if (cursorIdx === 3 && cursorRow === 7) {
+        clearInterval(interval.current);
+        return;
+      }
+
+      setCursorIdx(0);
+      setCursorRow((prev) => prev + 1);
+    }
+  }, [cursorIdx]);
+
+  useEffect(() => {
+    console.log({ cursorIdx, cursorRow });
+  }, [cursorIdx, cursorRow]);
+
+  return (
+    <div>
+      {codeTable.map((line, i) => (
+        <div key={i}>
+          {line.map((char, j) => {
+            const [row, col] = char.id.split("-").map(Number);
+            if (cursorIdx === col && cursorRow === row) {
+              return <div className="inline-block w-2 h-4 bg-white"></div>;
+            } else if (cursorRow < row || (cursorRow === row && cursorIdx < col)) {
+              return <div className="inline-block w-2 h-4"></div>;
+            } else if (row === 7 && col === 2) {
+              return <div className="inline-block w-2 h-4 bg-white animate-pulse"></div>;
+            } else {
+              return (
+                <span key={char.id} className={`text-${char.color} ${char.char === " " && j < 3 ? "ml-2" : ""} `}>
+                  {char.char}
+                </span>
+              );
+            }
+          })}
+        </div>
+      ))}
+    </div>
+  );
 }
