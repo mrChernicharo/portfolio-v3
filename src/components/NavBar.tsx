5aec88b5a109f563b3b7895c5dea8d0ae05d12ec
Felipe Chernicharo <string7dev@gmail.com>
2023-06-19 18:55:42 -0300

    using my pc again

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,15 +7,30 @@ import { Link } from "react-router-dom";
 
 export function NavBar() {
   return (
-    <div className="p-2 flex justify-between items-center bg-secondary text-secondary-content">
-      <div>Felipe Chernicharo</div>
+    <>
+      {/* nav-bar */}
+      <div className="fixed top-0 w-full h-16 px-6 flex justify-between items-center bg-secondary text-secondary-content">
+        <div className="w-[220px]">Felipe Chernicharo</div>
 
-      <Link to="/">Home</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+        <div>
+          <Link to="/">
+            <span className="mx-2">Home</span>
+          </Link>
+          <Link to="/projects">
+            <span className="mx-2">Projects</span>
+          </Link>
+          <Link to="/about">
+            <span className="mx-2">About</span>
+          </Link>
+          <Link to="/contact">
+            <span className="mx-2">Contact</span>
+          </Link>
+        </div>
 
-      <ThemeMenu />
-    </div>
+        <ThemeMenu />
+      </div>
+      {/* filler */}
+      <div className="bg-green-600 h-16"></div>
+    </>
   );
 }
